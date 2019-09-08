b2f393fc75481076f4a86d4c6da1e56554419b13
fmowl10 <jinseok1001@hotmail.com>
2019-09-08 10:34:16 +0000

    Improve UI/UX

--- frontend/src/chat.js
+++ frontend/src/chat.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import 'semantic-ui-css/semantic.min.css'
-import {Input, Form} from 'semantic-ui-react'
+import {Input, Form, Message, Segment} from 'semantic-ui-react'
 import {w3cwebsocket as W3CWebSocket} from "websocket";
 
 class Chat extends React.Component {
     constructor(props) {
         super(props)
-        this.state = {token : this.props.location.state.token, key : this.props.location.state.key, text : '', message : '', enable : false};
+        this.state = {token : this.props.location.state.token, key : this.props.location.state.key, text : '', message : '', enable : false, helpVisible: true};
         this.socket = new W3CWebSocket('wss://test.fmowl.com/ws', this.state.token);
     }
     componentWillMount() {
@@ -16,29 +16,94 @@ class Chat extends React.Component {
             this.socket.send(this.state.key)
         }
         this.socket.onmessage = (message) => {
-            this.setState({text : this.state.text +" \n"+ message.data})
+            this.setState({text : this.state.text + "\n" + message.data})
         }
         this.socket.onclose = () => {
-            this.setState({enable: false, text:'disconneted'})
+            this.setState({enable: false, text: this.state.text + "\ndisconneted"})
         }
     }
+
+    handleDismiss = () => {
+        this.setState({helpVisible: !this.state.helpVisible})
+    }
+
     handleSubmit = () =>{
+        const message = this.state.message
+        this.setState({message: ""})
+        if (message == "!help") {
+            this.setState({text: this.state.text +this.help, helpVisible : true})
+            return
+        }
+        if (message == "!clear") {
+            this.setState({text:""})
+            return
+        }
+        if (message == "!quit") {
+            this.socket.close()
+            return
+        }
+        if (message.length == 0) {
+            return
+        }
         this.socket.send(this.state.message)
     }
     handleChange = (e, {name, value}) => {
         this.setState({[name]: value});
     }
     render() {
+        const message = this.state.message
         return (
-            <div style={{backgroundColor:"#6b7572", padding:10, position:"absolute", top:0, right:0, bottom:0, left:0}}>
-                <div style={{backgroundColor:"#f7f7f7", padding:10, width:"100%", height:"100%"}}>
+            <div style={{
+                backgroundColor:"#6b7572",
+                padding:10,
+                position:"absolute",
+                top:0,
+                right:0,
+                bottom:0,
+                left:0}}>
+                
+                <div style={{
+                    backgroundColor:"#f7f7f7",
+                    padding:10,
+                    width:"100%",
+                    height:"100%"}}>
+                    {this.state.helpVisible &&
+                    <Message onDismiss={this.handleDismiss}>
+                        <Message.List>
+                            <Message.List>!help show help text</Message.List>
+                            <Message.List>!clear clear screen</Message.List>
+                            <Message.List>!quit quit this chat</Message.List>
+                        </Message.List>
+                    </Message>}
+                    <Message hidden={this.state.enable} error>
+                        <Message.Header>Disconnected with Server</Message.Header>
+                        if you want to reconnetction go https://test.fmowl.com/
+                        (if go back refresh it)
+                    </Message>
                     <label>You are {this.state.key}</label>
-                    <p style={{wordBreak:"break-all", wordWrap:"break-word", whiteSpace:"pre", width:200, heigth:"100%"}}>hello world<br/>{this.state.text}</p>
-                    <div style={{position:"relative",bottom:0, width:"100%"}}>
-                        <Form onSubmit={this.handleSubmit} >
-                            <div>
-                                <Input action="Submit" fluid disabled={!this.state.enable} onChange={this.handleChange} name="message" placeholder="input message"/>
-                            </div>
+                    <p style={{
+                        wordBreak:"break-all",
+                        wordWrap:"break-word",
+                        whiteSpace:"pre",
+                        width:"100%",
+                        overflow:"auto",
+                        height:"70%",
+                        border: "2px solid black",
+                        position: "inherit"
+                        }}>
+                        {this.state.text}
+                    </p>
+                    <div style ={{width:"100%"}}>
+                        <Form onSubmit={this.handleSubmit} autoComplete="off">
+                                <Input 
+                                    value = {message}
+                                    action="Submit"
+                                    fluid
+                                    disabled={!this.state.enable}
+                                    onChange={this.handleChange}
+                                    name="message"
+                                    placeholder="input message"
+                                />
                         </Form>
                     </div>
                 </div>
@@ -47,5 +112,4 @@ class Chat extends React.Component {
     }
 }
 
-//<!--<Button style={{width:"30%"}} isabled={!this.state.enable} content="Submit"/>-->
 export default Chat;
