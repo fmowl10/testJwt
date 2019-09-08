4824599bdad638f1f296962e7574fb6c19c3da80
fmowl10 <jinseok1001@hotmail.com>
2019-09-08 13:52:08 +0000

    Add comment to clients.go & util.go

--- backend/clients.go
+++ backend/clients.go
@@ -2,21 +2,23 @@ package backend
 
 import "log"
 
-//https://dev.to/danielkun/go-asynchronous-and-safe-real-time-broadcasting-using-channels-and-websockets-4g5d
-
+// Clients is collection of clients
+// Clients do sign / unsign and boradcast message
 type Clients struct {
 	clients   []chan<- string
 	singChan  chan chan string
 	unignChan chan chan string
 }
 
+// Broadcast send "s" all clients
 func (c Clients) Broadcast(s string) {
-	log.Println("client log: broadcasting: " + s)
+	log.Println(`client log: broadcasting: "` + s + `"`)
 	for _, v := range c.clients {
 		v <- s
 	}
 }
 
+// Init intialize CLients
 func (c *Clients) Init() {
 	log.Println("client log: Init")
 	c.clients = make([]chan<- string, 0, 10)
@@ -24,16 +26,19 @@ func (c *Clients) Init() {
 	c.unignChan = make(chan chan string)
 }
 
+// Sign add channel to c.clients
 func (c Clients) Sign(clientChan chan string) {
 	log.Println("client log: Signed")
 	c.singChan <- clientChan
 }
 
+// Unsign remove channel from c.clients and close channel
 func (c Clients) Unsign(clientChan chan string) {
 	log.Println("client log: Unsigned")
 	c.unignChan <- clientChan
 }
 
+// Hub do serve
 func (c *Clients) Hub() {
 	for {
 		log.Println("number of Chans: ", len(c.clients))
--- backend/util.go
+++ backend/util.go
@@ -7,12 +7,14 @@ import (
 	"github.com/dgrijalva/jwt-go"
 )
 
+// ResponseError write statusCode to header and responseBody to body
 func ResponseError(w http.ResponseWriter, statusCode int, responseBody string) {
 	w.WriteHeader(statusCode)
 	w.Write([]byte(responseBody))
 	return
 }
 
+// VaildToken check is rawToken vaild
 func VaildToken(rawToken string) bool {
 	token, _ := jwt.Parse(rawToken, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
